fe36e68202ce1f5abd3cb92179c0fcc5330adbe7
florianrc <56357894+florianrc@users.noreply.github.com>
2023-07-12 14:39:09 +0200

    First workshop implementation

---
+++ Simple_Arnet_LED/Arduino/Simple_Arnet_LED_0.1/Simple_Arnet_LED_0.1.ino
@@ -0,0 +1,76 @@
+#include <ArtnetWiFi.h>
+#include <FastLED.h>
+
+char ssid[] = "Livebox-8B20";
+char pass[] = "Raclette_1986";
+
+#define ID_BASE 200
+#define ID 0
+
+#define NUM_LEDS 64
+#define DATA_PIN 1
+
+IPAddress staticIP(192, 168, 1, ID_BASE+ID);
+IPAddress gateway(192, 168, 1, 254);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(192, 168, 1, 254);
+
+ArtnetWiFiReceiver artnet;
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only. You need to open monitor to leave this loop...
+  }
+  
+  Serial.println("");
+  Serial.println("Simple Artnet LED");
+  
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+  leds[0] = CRGB::Red;
+  FastLED.show();
+
+  if (!WiFi.config(staticIP, gateway, subnet, dns)) {
+    Serial.println("STA Failed to configure");
+  }
+  
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WIFI connection retry");
+    delay(100);
+  }
+  
+  Serial.println("WIFI connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  leds[0] = CRGB::Green;
+  FastLED.show();
+
+  artnet.begin();
+
+  artnet.subscribe([&](const uint32_t universe, const uint8_t* data, const uint16_t size)
+  {
+    Serial.println("artnet package");
+    int i;
+    for( i = 0; i < NUM_LEDS; i++ )
+    {
+      int pixel = 3*i;
+      leds[i].r = data[pixel];
+      leds[i].g = data[pixel+1];
+      leds[i].b = data[pixel+2];
+    }
+    FastLED.show();
+  });
+}
+
+void loop()
+{
+  artnet.parse();
+}
---
+++ Simple_OSC_ADC/Arduino/Simple_OSC_ADC_0.1/Simple_OSC_ADC_0.1.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+
+char ssid[] = "Livebox-8B20";
+char pass[] = "Raclette_1986";
+
+#define ID_BASE 200
+#define ID 1
+
+IPAddress staticIP(192, 168, 1, ID_BASE+ID);
+IPAddress gateway(192, 168, 1, 254);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(192, 168, 1, 254);
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+const IPAddress outIp(192,168,1,255);        // remote IP (not needed for receive)
+const unsigned int outPort = 9999;          // remote port (not needed for receive)
+const unsigned int localPort = 8888;        // local port to listen for UDP packets (here's where we send the packets)
+
+int sensorPin = 1;
+
+void setup()
+{
+  Serial.begin(115200);
+  /*
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only. You need to open monitor to leave this loop...
+  }
+  */
+  Serial.println("");
+  Serial.println("Simple OSC ADC");
+
+  if (!WiFi.config(staticIP, gateway, subnet, dns)) {
+    Serial.println("STA Failed to configure");
+  }
+  
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WIFI connection retry");
+    delay(100);
+  }
+  
+  Serial.println("WIFI connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+}
+
+void loop()
+{
+  int sensorValue = 0;
+  sensorValue = analogRead(sensorPin);
+  OSCMessage msg("/pressure");
+  msg.add(sensorValue);
+  Udp.beginPacket(outIp, outPort);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+
+  delay(100);
+}
